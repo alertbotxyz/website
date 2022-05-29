d6b82ad1e5ab913110ac45e64066aa82faff4fcc
thereal-atom <oscarfal2006@gmail.com>
2022-05-29 22:42:46 +0100

    update templates

---
+++ src/routes/dashboard/account.svelte
@@ -0,0 +1,21 @@
+<!-- Bot data:
+    token
+    avatar
+    description 
+    name
+-->
+<!-- 
+    Subscription data:
+    select:
+        free: cancel
+        paid
+        premium
+-->
+<!-- 
+    Billing history:
+    array:
+        date
+        description
+        amount
+        invoice
+ -->
