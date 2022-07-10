680b949572a7f9edcfea30dca465077d2b9cc4d5
thereal-atom <oscarfal2006@gmail.com>
2022-07-10 16:00:39 +0100

    fix cookie test 3

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -7,9 +7,24 @@ export const login = () => {
 };
 
 export const logout = () => {
-    if (document.cookie.includes("DISCORD_SESSION_ID")) {
-        document.cookie = "DISCORD_SESSION_ID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;SameSite=none;Secure=true;HttpOnly=true;path=/";
+    const getCookie = (name) => {
+        return document.cookie.split(";").some(c => {
+            return c.trim().startsWith(name + "=");
+        });
     };
+
+    const env = constants.env;
+
+    const deleteCookie = (name, path, domain) => {
+        if (getCookie(name)) {
+            document.cookie = name + "=" +
+                (path ? ";path=" + path : "") +
+                (domain ? ";domain=" + domain : "") +
+                `;expires=Thu, 01 Jan 1970 00:00:01 GMT;SameSite=None;secure=${env === "production"}`;
+        };
+    };
+
+    deleteCookie("DISCORD_SESSION_ID", "/", window.location.hostname);
     window.location.pathname = "/";
 
     return makeRequest("/discord/logout");
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,5 @@ export default {
     api: {
         url: `${import.meta.env.PROD ? "https://api.alert-bot.xyz" : "http://localhost:50451"}/v1`,
     },
+    env: import.meta.env.PROD ? "production" : "development",
 };
